9d3bdc2f7b9623c281754ff24b750ed37db46336
SlightlyLessBigJimmy <beenclarke1@gmail.com>
2024-06-01 21:59:41 -0400

    inventory1

--- Assets/scripts/InventoryManager.cs
+++ Assets/scripts/InventoryManager.cs
@@ -4,8 +4,25 @@ using UnityEngine;
 
 public class InventoryManager : MonoBehaviour
 {
+    public GameObject InventoryUI;
+
     // Start is called before the first frame update
     void Start()
+    {
+        InventoryUI.SetActive(false);
+    }
+
+    public void GiveItem(itemName, amount)
+    {
+        
+    }
+
+    public void RemoveItem(itemName, amount)
+    {
+        
+    }
+
+    public void HasItem(itemName)
     {
         
     }
@@ -13,6 +30,17 @@ public class InventoryManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            if (InventoryUI.activeSelf == true)
+            {
+                InventoryUI.SetActive(false);
+            }
+            else if (InventoryUI.activeSelf == false)
+            {
+                InventoryUI.SetActive(true);
+            }
+            
+        }
     }
 }
